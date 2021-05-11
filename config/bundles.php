3ff42ec8fb1f60954df493e35ae602b51e6c155f
Myogamevideo <alexandre.bougrat@hotmail.fr>
2021-05-11 09:47:16 +0200

    add webpack

--- config/bundles.php
+++ config/bundles.php
@@ -3,4 +3,11 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    FOS\UserBundle\FOSUserBundle::class => ['all' => true],
 ];
--- webpack.config.js
+++ webpack.config.js
@@ -20,10 +20,10 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
+    //.addEntry('app', './assets/app.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
+    //.enableStimulusBridge('./assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -45,9 +45,9 @@ Encore
     // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
 
-    .configureBabel((config) => {
-        config.plugins.push('@babel/plugin-proposal-class-properties');
-    })
+    // .configureBabel((config) => {
+    //     config.plugins.push('@babel/plugin-proposal-class-properties');
+    // })
 
     // enables @babel/preset-env polyfills
     .configureBabelPresetEnv((config) => {
@@ -55,11 +55,19 @@ Encore
         config.corejs = 3;
     })
 
+    .configureBabel(() => {
+    }, {
+        useBuiltIns: 'usage',
+        corejs: 3
+    })
+
+
     // enables Sass/SCSS support
     .enableSassLoader()
 
     // uncomment if you use TypeScript
     .enableTypeScriptLoader()
+    .enableVueLoader()
 
     // uncomment if you use React
     //.enableReactPreset()
@@ -70,6 +78,27 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+    .addLoader({
+        test: /\.(html)$/,
+        use: {
+            loader: 'html-loader',
+        },
+    })
+
+    .copyFiles({
+        from: './assets/front/images',
+        to: 'images/front/[path][name].[ext]'
+    })
+
+    .addExternals({
+        tinymce: 'tinymce',
+    })
+
+    //Frontend style and script
+    .addEntry('front-scripts', './assets/front/scripts/index.ts')
+    .addStyleEntry('front-style', './assets/front/styles/index.scss')
+    .addStyleEntry('front-libs', './assets/front/styles/libs.scss')
 ;
 
 module.exports = Encore.getWebpackConfig();
