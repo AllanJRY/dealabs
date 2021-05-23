4303685f14e95c6c809f2e954693a6307c6ea804
Myogamevideo <alexandre.bougrat@hotmail.fr>
2021-05-23 12:45:28 +0200

    home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,41 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
+use App\Repository\DealRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
+
+    /**
+     * @var CategoryRepository
+     */
+    private $categoryRepository;
+    /**
+     * @var DealRepository
+     */
+    private $dealRepository;
+
+    public function __construct(CategoryRepository $categoryRepository, DealRepository $dealRepository)
+    {
+        $this->categoryRepository = $categoryRepository;
+        $this->dealRepository = $dealRepository;
+    }
+
     /**
      * @Route("/", name="home")
      */
     public function index(): Response
     {
+        $categories = $this->categoryRepository->findAll();
+        $deals = $this->dealRepository->findAll();
+
         return $this->render('pages/home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'categories' => $categories,
+            'deals' => $deals,
         ]);
     }
 }
