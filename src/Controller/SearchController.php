8321ef7a7efd186d246c9d2c103586ed1a47b911
AllanJRY <jarry.allan@gmail.com>
2021-06-15 15:56:50 +0200

    Created search controller

---
+++ src/Controller/SearchController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Deal;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    /**
+     * SearchController constructor.
+     * @param EntityManagerInterface $entityManager
+     */
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+
+    /**
+     * @Route({"en": "/search", "fr": "/recherche"}, name="search")
+     */
+    public function index(Request $request): Response
+    {
+        $query = $request->query->get('s');
+        return $this->render('pages/search/index.html.twig', [
+            'controller_name' => 'SearchController',
+        ]);
+    }
+}
