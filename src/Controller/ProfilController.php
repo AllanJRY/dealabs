5f9bf5ce3847e2179c5f4a5472749a1d12a1588f
AllanJRY <jarry.allan@gmail.com>
2021-06-15 15:25:00 +0200

    [FEAT] Listing user published deals on profil

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,6 +26,22 @@ class ProfilController extends AbstractController
     public function index(): Response
     {
         return $this->render('pages/profil/index.html.twig', [
+        ]);
+    }
+
+    /**
+     * @Route({"en": "/my-deals", "fr": "/mes-deals"}, name="profil_published_deals")
+     */
+    public function publishedDeals(Request $request, PaginatorInterface $paginator): Response
+    {
+        $userPublishedDeals = $paginator->paginate(
+            $this->getUser()->getDeals(),
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('pages/profil/published_deals.html.twig', [
+            "user_published_deals" => $userPublishedDeals,
         ]);
     }
 }
